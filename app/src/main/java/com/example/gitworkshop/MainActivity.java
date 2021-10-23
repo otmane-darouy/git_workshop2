68c968c780596708ee1292359bcd76a784b7432d
DAROUY Otmane <contact.optimasystem@gmail.com>
2021-10-23 11:46:22 +0100

    commit #4

--- app/src/main/java/com/example/gitworkshop/MainActivity.java
+++ app/src/main/java/com/example/gitworkshop/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//test
+//L1
+    //L2
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
